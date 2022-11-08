09a9695dc627c07e807544c71024d1a54ecdae35
Tylerdays <tylerdday.software@gmail.com>
2022-11-07 21:15:59 -0700

    Small changes to the script

---
+++ js/app.js
@@ -0,0 +1,18 @@
+let wasTweet = {
+    
+    
+    past: [
+    {
+    tweet: [`Some news`],
+    userName: 'Somebody1',
+    date: `11-6-22`,
+    age: 18,
+    }
+]
+}
+
+if ((wasTweet[`age`] = 18)){
+    console.log(wasTweet[past]);
+} else {
+    console.log(`can't find tweet`)
+}
